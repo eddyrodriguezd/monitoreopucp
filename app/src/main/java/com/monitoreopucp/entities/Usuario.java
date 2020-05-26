9f73b84110176de08f2deed06e0d419b61d5a066
Eddy Rodriguez <e.rodriguezd@pucp.edu.pe>
2020-05-26 12:57:28 -0500

    Usuario e Incidencia creados

--- app/src/main/java/com/monitoreopucp/entities/Usuario.java
+++ app/src/main/java/com/monitoreopucp/entities/Usuario.java
@@ -1,4 +1,60 @@
 package com.monitoreopucp.entities;
 
 public class Usuario {
+    private int id;
+    private String nombre;
+    private String apellido;
+    private String correo;
+    private int codigo;
+
+    public Usuario() {
+    }
+
+    public Usuario(int id, String nombre, String apellido, String correo, int codigo) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.correo = correo;
+        this.codigo = codigo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
 }
