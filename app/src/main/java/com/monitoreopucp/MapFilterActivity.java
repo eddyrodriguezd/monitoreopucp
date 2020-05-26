469d0ca0e45ea12158c9455486f66f13c4129474
Eddy Rodriguez <e.rodriguezd@pucp.edu.pe>
2020-05-26 17:06:21 -0500

    MapFilterActivity

---
+++ app/src/main/java/com/monitoreopucp/MapFilterActivity.java
@@ -0,0 +1,33 @@
+package com.monitoreopucp;
+
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapFilterActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map_filter);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        LatLng pucp = new LatLng(-12.069512,-77.0815479);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(pucp, 15f));
+    }
+}
