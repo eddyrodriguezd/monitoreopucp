501e1e95e4077befaaa38e47f5f13467398e388b
Jose Ruiz <ruizn.ja@pucp.pe>
2020-05-26 23:25:22 -0500

    Formulario Incidencia - IDs

--- app/src/main/java/com/monitoreopucp/IncidenciaFormulario.java
+++ app/src/main/java/com/monitoreopucp/IncidenciaFormulario.java
@@ -10,5 +10,6 @@ public class IncidenciaFormulario extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_incidencia_formulario);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
