957e3870f5e77e88405224ef886ea444cc1999bd
Jose Ruiz <ruizn.ja@pucp.pe>
2020-05-26 20:59:06 -0500

    Incidencias Cercanas - App bar XML

--- app/src/main/java/com/monitoreopucp/IncidenciasListActivity.java
+++ app/src/main/java/com/monitoreopucp/IncidenciasListActivity.java
@@ -1,14 +1,55 @@
 package com.monitoreopucp;
 
+import androidx.activity.OnBackPressedCallback;
+import androidx.activity.OnBackPressedDispatcher;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class IncidenciasListActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_nueva_incidencia,menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.menuItem_NuevaIncidencia:
+                return true;
+            default:
+                Intent intent = new Intent();
+                setResult(RESULT_OK,intent);
+                finish();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_incidencias_list);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        OnBackPressedCallback callback = new OnBackPressedCallback(true) {
+            @Override
+            public void handleOnBackPressed() {
+                Intent intent = new Intent();
+                setResult(RESULT_OK,intent);
+                finish();
+            }
+        };
+
     }
+
+
 }
