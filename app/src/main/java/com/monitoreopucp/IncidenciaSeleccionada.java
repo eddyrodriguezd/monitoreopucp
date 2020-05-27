54545e8f9dc0cc276ceda45b4d7629bbe8b0dad9
Jose Ruiz <ruizn.ja@pucp.pe>
2020-05-26 22:04:15 -0500

    Incidencias Seleccionada - App Bar + retorno

--- app/src/main/java/com/monitoreopucp/IncidenciaSeleccionada.java
+++ app/src/main/java/com/monitoreopucp/IncidenciaSeleccionada.java
@@ -1,14 +1,41 @@
 package com.monitoreopucp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class IncidenciaSeleccionada extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_incidencia_seleccionada, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.itemMenu_AgregarAnotacion:
+                return true;
+            default:
+                Intent intent = new Intent();
+                setResult(RESULT_OK,intent);
+                finish();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_incidencia_seleccionada);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
