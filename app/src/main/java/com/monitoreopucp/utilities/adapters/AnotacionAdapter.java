5837479ec2ddad24a57858eb92b686fef0750a51
Jose Ruiz <ruizn.ja@pucp.pe>
2020-06-30 23:45:33 -0500

    CAMBIO TITULO ANOTACIONES

--- app/src/main/java/com/monitoreopucp/utilities/adapters/AnotacionAdapter.java
+++ app/src/main/java/com/monitoreopucp/utilities/adapters/AnotacionAdapter.java
@@ -9,13 +9,20 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.monitoreopucp.R;
 import com.monitoreopucp.entities.Anotacion;
+import com.monitoreopucp.entities.Usuario;
 
 public class AnotacionAdapter extends RecyclerView.Adapter<AnotacionAdapter.AnotacionViewHolder> {
 
     private Anotacion[] data;
     private Context context;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     public AnotacionAdapter(Anotacion[] data, Context context) {
         this.data = data;
@@ -47,10 +54,27 @@ public class AnotacionAdapter extends RecyclerView.Adapter<AnotacionAdapter.Anot
 
     @Override
     public void onBindViewHolder(AnotacionViewHolder holder, int position) {
-        String titulo = String.valueOf(data[position].getIdUsuario());
-        String cuerpo = String.valueOf(data[position].getContenido());
+        final Anotacion[] anotacion = {data[position]};
+        final String[] titulo = new String[1];
+        String cuerpo;
 
-        holder.textTitulo.setText(titulo);
+        db.collection("usuarios").document(data[position].getIdUsuario()).
+                get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    Usuario usuario = document.toObject(Usuario.class);
+                    titulo[0] = usuario.getNombre() + ", " + usuario.getApellido();
+                }
+                else {
+                    titulo[0] = String.valueOf(anotacion[0].getIdUsuario());
+                }
+            }
+        });
+        cuerpo = String.valueOf(anotacion[0].getContenido());
+
+        holder.textTitulo.setText(titulo[0]);
         holder.textCuerpo.setText(cuerpo);
 
     }
