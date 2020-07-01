2a2420b4425eab8bed015b9d0e3935bc6afa4221
Eddy Rodriguez <e.rodriguezd@pucp.edu.pe>
2020-07-01 12:06:55 -0500

    login w/Firebase

--- app/src/main/java/com/monitoreopucp/entities/Usuario.java
+++ app/src/main/java/com/monitoreopucp/entities/Usuario.java
@@ -8,16 +8,18 @@ public class Usuario implements Serializable {
     private String apellido;
     private String correo;
     private int codigo;
+    private boolean infra;
 
     public Usuario() {
     }
 
-    public Usuario(int id, String nombre, String apellido, String correo, int codigo) {
+    public Usuario(int id, String nombre, String apellido, String correo, int codigo, boolean infra) {
         this.id = id;
         this.nombre = nombre;
         this.apellido = apellido;
         this.correo = correo;
         this.codigo = codigo;
+        this.infra = infra;
     }
 
     public int getId() {
@@ -58,5 +60,13 @@ public class Usuario implements Serializable {
 
     public void setCodigo(int codigo) {
         this.codigo = codigo;
+    }
+
+    public boolean isInfra() {
+        return infra;
+    }
+
+    public void setInfra(boolean infra) {
+        this.infra = infra;
     }
 }
